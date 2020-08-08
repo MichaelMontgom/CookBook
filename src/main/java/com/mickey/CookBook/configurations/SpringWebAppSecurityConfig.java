bb08ed6f8e101bcc49d07bdf11a8160191f2c87f
MichaelMontgom <mem22695@gmail.com>
2020-08-08 17:31:19 -0400

    Created models and passed info to views

--- src/main/java/com/mickey/CookBook/configurations/SpringWebAppSecurityConfig.java
+++ src/main/java/com/mickey/CookBook/configurations/SpringWebAppSecurityConfig.java
@@ -1,22 +1,22 @@
-package com.mickey.CookBook.configurations;
-
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@EnableOAuth2Sso
-@Configuration
-public class SpringWebAppSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .anyRequest().authenticated();
-
-
-    }
-
-}
+//package com.mickey.CookBook.configurations;
+//
+//import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@EnableOAuth2Sso
+//@Configuration
+//public class SpringWebAppSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http.authorizeRequests()
+//                .antMatchers("/").permitAll()
+//                .anyRequest().authenticated();
+//
+//
+//    }
+//
+//}
